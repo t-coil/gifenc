307395c0af6e9130914e8477eac8d1fe1bb14c84
Tessa <tessa@discordapp.com>
2021-08-06 14:53:03 -0700

    dist

--- dist/gifenc.js
+++ dist/gifenc.js
@@ -695,7 +695,9 @@ function GIFEncoder(opt = {}) {
         palette = null,
         repeat = 0,
         colorDepth = 8,
-        dispose = -1
+        dispose = -1,
+        x = 0,
+        y = 0
       } = opts;
       let first = false;
       if (auto) {
@@ -722,7 +724,7 @@ function GIFEncoder(opt = {}) {
       const delayTime = Math.round(delay / 10);
       encodeGraphicControlExt(stream, dispose, delayTime, transparent, transparentIndex);
       const useLocalColorTable = Boolean(palette) && !first;
-      encodeImageDescriptor(stream, width, height, useLocalColorTable ? palette : null);
+      encodeImageDescriptor(stream, width, height, x, y, useLocalColorTable ? palette : null);
       if (useLocalColorTable)
         encodeColorTable(stream, palette);
       encodePixels(stream, index, width, height, colorDepth, accum, htab, codetab);
@@ -791,10 +793,10 @@ function encodeColorTable(stream, palette) {
     stream.writeByte(color[2]);
   }
 }
-function encodeImageDescriptor(stream, width, height, localPalette) {
+function encodeImageDescriptor(stream, width, height, x, y, localPalette) {
   stream.writeByte(44);
-  writeUInt16(stream, 0);
-  writeUInt16(stream, 0);
+  writeUInt16(stream, x);
+  writeUInt16(stream, y);
   writeUInt16(stream, width);
   writeUInt16(stream, height);
   if (localPalette) {
